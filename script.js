ea281c2add338c2f1aa679eb0d8f36d98f995873
OSCA98 <oscarschwarz98@gmail.com>
2023-11-03 10:45:07 +0100

    Comments

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+//Store specific elements in variables
 const button = document.getElementById('jsTestElement');
 const counter = document.getElementById('jsCounter');
 
+//Add clickevent to button-element
 button.addEventListener('click', () => {
+  //When click, call function 'increase'
   increase();
 })
 
 const increase = () => {
+  //Get current counter-value as number
   const currentValue = parseInt(counter.innerHTML);
+  //Increase value by one
   counter.innerHTML = currentValue+1;
 }

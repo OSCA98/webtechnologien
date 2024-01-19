a9d853d56a7c12e9deba4bc1da3b410bab16c5b5
OSCA98 <oscarschwarz98@gmail.com>
2024-01-19 11:33:38 +0100

    Comments

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
+/**
+ * When JS is deactivated, we want the quizOption not to appear in the header
+ * Therefore we inject this option via JS
+ */
 function injectQuizLink() {
   const quizLink = document.createElement('a');
   quizLink.href = "Quiz.html"
   quizLink.title= "Zur Seite Weltraum-Quiz"
   quizLink.innerHTML = "Weltraum-Quiz";
   const headerElement = document.getElementById('header');
+  //Inject element before children[1] ('Impressum&Kontakt)
   headerElement.insertBefore(quizLink,headerElement.children[1])
 }
 
